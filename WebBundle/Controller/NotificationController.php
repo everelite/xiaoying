229257db5e262574e9094448812965f2803448c3
dragon <358211133@qq.com>
2017-11-07 13:07:23 +0800

    add page

--- WebBundle/Controller/NotificationController.php
+++ WebBundle/Controller/NotificationController.php
@@ -19,8 +19,10 @@ class NotificationController extends BaseController
         $paginator = new Paginator(
             $request,
             $this->getNotificationService()->getUserNotificationCount($user->id),
-            20
+            5
         );
+
+        
 
         // 获取个人消息通知
         $notifications = $this->getNotificationService()->findUserNotifications(
@@ -29,8 +31,15 @@ class NotificationController extends BaseController
             $paginator->getPerPageCount()
         );
 
+        // 执行分页程序
+        $paginators = new Paginator(
+            $request,
+            count($nots = $this->getNotification()),
+            5
+        );
+
         // 获取群发消息通知
-        $nots = $this->getNotification();
+        $nots = $this->getNotification($paginators->getOffsetCount(), $paginators->getPerPageCount());
 
         // 修改
         for ($i=0; $i < count($nots); $i++) { 
@@ -45,6 +54,7 @@ class NotificationController extends BaseController
         return $this->render('TopxiaWebBundle:Notification:index.html.twig', array(
             'notifications' => $notifications,
             'paginator' => $paginator,
+            'paginators' => $paginators,
             'nots' => $nots
         ));
     }
@@ -80,12 +90,12 @@ class NotificationController extends BaseController
     /**
     * 获取指定条数的群发消息，默认10条
     */
-    protected function getNotification($limit = 10)
+    protected function getNotification($start = 0, $limit = 10)
     {
         // 连接数据库，获取PDO对象
         $pdo = new \PDO('mysql:host=127.0.0.1;dbname=wangxiao', 'root', 'root');
         // 准备SQL
-        $sql = "select * from batch_notification ORDER BY `createdTime` DESC limit 0,{$limit}";
+        $sql = "select * from batch_notification ORDER BY `createdTime` DESC limit {$start},{$limit}";
         $stmt = $pdo->prepare($sql);
         // 执行操作
         $stmt->execute();
